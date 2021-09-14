b0b07ebcdc369831d9c7c6ffabbdde061faeba12
Eunsung Moon <eunsung9647@gmail.com>
2021-09-14 20:30:43 +0900

    날짜 함수 정리, 날짜 클릭시 강조 그러나 다른날짜 클릭은 안됨

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-/***************************************
-날짜 관련 함수 정리(공부한거 까먹으면 아깝자나....ㅜ)
+/******************************************************************************************
+-날짜 관련 함수 정리(공부한거 까먹으면 아깝자나....ㅜ)
 getFullYear() : 연도 반환
 getMonth() : 월 반환 1월-12월 -> 0-11 (해당하는 월을 출력하고 싶을때 getMonth()+1로 해줌)
 getDay() : sunday-saturday -> 0-6
 getDate() : 일 반환
-***************************************/
+-버튼 타입에 관련한 내용(새로 알게 된 사실이라 적어둠!!)
+버튼 타입 3가지 : submit, button, reset
+submit : 타입 명시가 없다면 기본 타입. form 테그로 버튼이 감싸져있을 경우 
+        버튼 클릭 시 새로고침 됨 => onsubmit ="return false" 해주면 해결
+
+        **마크업 측면에서 버튼의 기능을 명시해주는 것이 좋다
+button : 클릭해도 기능없음. 기능함수를 만들면 기능을 실행하기 위한 대상의 역할
+reset : 버튼을 감싸고 있는 form 데이터의 입력된 데이터를 초기화
+*****************************************************************************************/
 /**
 todolist
-1. 날짜 클릭 -> 색 바뀌기, 날짜, 요일 바뀌기
-2. todolist 만들기
+1. 날짜 클릭 -> 색 바뀌기, 날짜, 요일 바뀌기, 다른 날짜 선택 시 이전선택 날짜 글씨 색 바꾸기
+2. for문 foreach로 바꾸기
+3. 날짜 클릭 이벤트, 클릭한 날짜와 요일 바꾸기
 **/
 
 let el = selector => document.querySelector(selector);
@@ -47,6 +56,7 @@ let showCalendar = () => {
       //빈칸을 만드려면 빈칸에 해당하는 조건을 만드는게 당연하지!!
       //시작요소 : 첫번째 주라면 시작하는 요일부터 날짜를 출력하기 위한 설정 
       //마지막요소 : 월마다 총 일수가 다르기 때문에 pageYear에 월마다 일수를 넣음. 비교하면서 출력
+
       if ((i === 0 && j < first.getDay()) || cnt > pageYear[first.getMonth()]) {
         let $td = dom('td');
         $tr.appendChild($td);
@@ -81,28 +91,27 @@ let removeCalendar = () => {
 //이전, 다음 월 계산
 let calendarCal = e => {
   today = new Date(today.getFullYear(), today.getMonth() - e, today.getDate());
-  if (pageFirst.getMonth() === 1) {
-    pageFirst = new Date(first.getFullYear() - e, 12, 1);
+  pageFirst = new Date(first.getFullYear(), first.getMonth() - e, 1);
+  first = pageFirst;
 
-  } else {
-    pageFirst = new Date(first.getFullYear(), first.getMonth() - e, 1);
-    first = pageFirst;
-  }
 }
 
+//이전, 다음 월 이동 버튼
 let btnEvt = () => {
   el('.btn-wrap').addEventListener('click', e => {
     if (e.target.id == 'prev') {
+      removeCalendar();
       calendarCal(1)
       showMonth();
-      removeCalendar();
       showCalendar();
+      showMain();
 
     } else if (e.target.id == 'next') {
+      removeCalendar();
       calendarCal(-1)
       showMonth();
-      removeCalendar();
       showCalendar();
+      showMain();
     }
   })
 }
@@ -115,33 +124,56 @@ let showMain = () => {
   mainTodayDate.innerHTML = today.getDate();
 
   let todayDate = document.getElementById(today.getDate());
-  todayDate.classList.add('active');
-  todayDate.style.color = 'red'
-
+  addActive(todayDate);
 }
 
-//날짜 클릭 -> 그 날짜, 요일 출력 =>보류
-// let clickToday = e => {
-//   let group=document.getElementById(pageYear[first.getMonth()])
-//   console.log(group);
-// }
-// clickToday();
+//날짜 클릭 -> 그 날짜, 요일 출력 =>다음이 안되네
+let clickToday = () => {
+  el('.calendar-body').addEventListener('click', e => {
+    let todaySel = document.getElementById(today.getDate());
 
-let addList = () => { //엔터도 동작하고싶어여
-  el('.input-data').addEventListener('click', () => {
-    if (el('.input-box').value.length == 0) {
-      alert('Please enter a task');
-    } else {
-      el('.input-list ul').innerHTML +=
-        `<li>${el('.input-box').value} <button class="delText">X</button></li>`
+    if (e.target.id) {
+      removeActive(todaySel);
+      addActive(e.target);
     }
-    el('.input-box').value = '';
   })
 }
 
+//active class 삭제 
+let removeActive = e => {
+  if (e.className == 'active') {
+    e.classList.remove('active');
+    e.style.color = '';
+  }
+}
+
+//active class 추가
+let addActive = e => {
+  e.classList.add('active');
+  e.style.color = 'red';
+}
+
+//todolist 추가
+let addList = () => {
+  let $inputBox = el('.input-box');
+  if ($inputBox.value.length == 0) {
+    alert('Please enter a task');
+  } else {
+    el('.input-list ul').innerHTML +=
+      `<li>${$inputBox.value} <button class="delText">X</button></li>`
+  }
+  $inputBox.value = '';
+}
+
+//todolist 추가 이벤트 함수
+let addListFunc = () => {
+  el('.input-data').addEventListener('click', addList);
+}
+
+//list 삭제 
 let delEvt = () => {
-  el('.delText').addEventListener('click', ()=>{
-    el('.list li').remove()
+  el('.list').addEventListener('click', e => {
+    e.target.className ? e.target.parentNode.remove() : '';
   })
 }
 
@@ -150,6 +182,8 @@ let init = () => {
   showMonth();
   btnEvt();
   showMain();
-  addList();
+  addListFunc();
+  delEvt();
+  clickToday();
 }
 init();
